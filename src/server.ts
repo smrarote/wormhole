e611f82a3f7b1ed35e54fec94b512f2b7ec8eddd
Sameer Arote <smrarote@gmail.com>
2024-03-11 20:24:12 +0530

    test scrips

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("hi bhau");
+import http from "node:http";
+import app from "./app.js";
+let server = http.createServer(app);
+
+server
+  .listen(process.env.PORT || 3000)
+  .on("listening", () =>
+    console.log(`🟢 Running  : ${process.env.PORT || 3000}`)
+  );
+
+server.close(() => console.log("🔴 Stopped"));
